02f4b67e13b5b7cfc7b8e5151432ccf8b660921c
Albert Noll <noll.albert@gmail.com>
2019-03-17 21:54:17 +0100

    initial import

---
+++ terraform/excercise_2/adw.tf
@@ -0,0 +1,47 @@
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "compartment_ocid" {}
+variable "region" {}
+
+provider "oci" {
+  version          = ">= 3.0.0"
+  tenancy_ocid     = "${var.tenancy_ocid}"
+  user_ocid        = "${var.user_ocid}"
+  fingerprint      = "${var.fingerprint}"
+  private_key_path = "${var.private_key_path}"
+  region           = "${var.region}"
+}
+
+
+resource "random_string" "adw_admin_password" {
+  length = 16
+  special = true
+}
+
+resource "oci_database_autonomous_data_warehouse" "adw" {
+  admin_password             = "${random_string.adw_admin_password.result}"
+  compartment_id             = "${var.compartment_ocid}"
+  cpu_core_count             = "${var.adw_cpu_count}"
+  data_storage_size_in_tbs   = "${var.adw_data_storage_size_in_tb}"
+  db_name                    = "${var.adw_db_name}"
+  db_name                    = "sepp"
+}
+
+data "oci_database_autonomous_data_warehouses" "adws" {
+  compartment_id = "${var.compartment_ocid}"
+}
+
+output "adw_admin_password" {
+  value = "${random_string.adw_admin_password.result}"
+}
+
+/*
+output "autonomous_data_warehouses" {
+  value = "${data.oci_database_autonomous_data_warehouses.adws.autonomnous_data_warehouses}"
+}
+*/
+output "parallel_connection_string" {
+  value = ["${lookup(oci_database_autonomous_data_warehouse.adw.connection_strings.0.all_connection_strings, "PARALLEL", "Unavailable")}"]
+}
